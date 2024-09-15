ae33a2741df041099f488655301390daec15cfb5
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-09-15 16:41:22 +0500

    update the changes

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const button = document.getElementById('toggleButton');
+const backgroundbody = document.getElementById("darkmode");
+backgroundbody.style.backgroundColor = "white";
+button.style.color = "black";
 let isDay = true;
-button.addEventListener('click', function(){
-    if (isDay) {
-        document.body.style.backgroundColor = 'black';
-        button.textContent = 'Night';
-    } else {
-        document.body.style.backgroundColor = 'white';
+
+button.addEventListener('click', function() {
+    if (backgroundbody.style.backgroundColor === "black") {
+        backgroundbody.style.backgroundColor = 'white';
         button.textContent = 'Day';
-    
+    } else {
+        backgroundbody.style.backgroundColor = 'black';
+        button.textContent = 'Night';
     }
-    
-    
-})
+});
+
